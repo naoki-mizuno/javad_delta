29e29454ca59f6e0b182548afff8650990c5081c
Naoki Mizuno <mizuno.naoki@rm.is.tohoku.ac.jp>
2019-01-08 12:41:41 +0900

    Rename gps_* to gnss_*

--- nodes/javad_delta_node.py
+++ nodes/javad_delta_node.py
@@ -17,7 +17,7 @@ def publish_gga(gga, configs):
 
 
     msg = Sentence()
-    msg.header.frame_id = configs['gps_origin_frame']
+    msg.header.frame_id = configs['gnss_origin_frame']
     msg.header.stamp = rospy.Time.now()
     msg.sentence = gga
     if javad_delta.GGA.is_good_gga(msg.sentence):
@@ -27,8 +27,8 @@ def publish_gga(gga, configs):
 
 def send_transform(msg, configs):
     fix = javad_delta.GGA(msg)
-    transformer = javad_delta.CoordTransformer(configs['gps_src_epsg'],
-                                               configs['gps_tgt_epsg'])
+    transformer = javad_delta.CoordTransformer(configs['gnss_src_epsg'],
+                                               configs['gnss_tgt_epsg'])
     x, y, z = transformer.transform(fix)
     translation = (x, y, z)
     # No orientation
@@ -37,8 +37,8 @@ def send_transform(msg, configs):
     tf_b.sendTransform(translation,
                        rotation,
                        msg.header.stamp,
-                       configs['gps_antenna_frame'],
-                       configs['gps_origin_frame'])
+                       configs['gnss_antenna_frame'],
+                       configs['gnss_origin_frame'])
 
 
 
@@ -47,14 +47,14 @@ def main():
 
     ros_configs = {
         'publish_tf': rospy.get_param('~publish_tf', True),
-        'gps_origin_frame': rospy.get_param('~gps_origin_frame', 'gps_origin'),
-        'gps_antenna_frame': rospy.get_param('~gps_antenna_frame', 'gps_antenna'),
+        'gnss_origin_frame': rospy.get_param('~gnss_origin_frame', 'gnss_origin'),
+        'gnss_antenna_frame': rospy.get_param('~gnss_antenna_frame', 'gnss_antenna'),
         # Required data quality to publish tf
-        'gps_required_quality': rospy.get_param('~gps_required_quality', ['4', '5']),
+        'gnss_required_quality': rospy.get_param('~gnss_required_quality', ['4', '5']),
         # Uses WGS84 by default
-        'gps_src_epsg': rospy.get_param('~gps_src_epsg', 4326),
+        'gnss_src_epsg': rospy.get_param('~gnss_src_epsg', 4326),
         # Earth Centered, Earth Fixed
-        'gps_tgt_epsg': rospy.get_param('~gps_tgt_epsg', 4978),
+        'gnss_tgt_epsg': rospy.get_param('~gnss_tgt_epsg', 4978),
     }
     ntrip_configs = {
         'enable': rospy.get_param('~ntrip_enable'),
@@ -75,7 +75,7 @@ def main():
         gga_sentence = client.read_gga_sentence()
         published_msg = publish_gga(gga_sentence, ros_configs)
         is_good_gaa = javad_delta.GGA.is_good_gga(gga_sentence,
-                                                ros_configs['gps_required_quality'])
+                                                ros_configs['gnss_required_quality'])
         if ros_configs['publish_tf'] and is_good_gaa:
             send_transform(published_msg, ros_configs)
 
